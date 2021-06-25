f4e8ba9e770ec21d72521958555c480e547593a1
slabad <dmslabaugh@gmail.com>
2021-06-25 16:45:05 -0400

    Update class_8_homework.sql

--- class_8_homework.sql
+++ class_8_homework.sql
@@ -14,3 +14,20 @@ set delivery=1
 from orders 
 join customers on orders.customer_id=customers.id 
 where first_name='Bob' and last_name='Smith'
+
+--Change the state for all the customers who had an order that cost more than $100
+update customers
+set state = 'CA'
+from orders 
+join customers on customers.id = orders.customer_id
+where orders.cost > 100;
+
+--Change the topping of onions to anchovies on three orders from bexley
+update top(100) orders_toppings 
+set topping_id = (select id from toppings where name = 'anchovies')
+from customers 
+join orders on customers.id = orders.customer_id
+join orders_toppings on orders.id = orders_toppings.order_id
+join toppings on toppings.id = orders_toppings.topping_id
+where toppings.name = 'onion'
+  and city = 'Bexley';
